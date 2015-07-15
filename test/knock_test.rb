f5380949dc31f64d835eb1c8792b9bd04f6eeef5
Arnaud MESUREUR <arnaud.mesureur@gmail.com>
2015-07-15 18:14:32 +0200

    add missing test for Knock.setup method

--- test/knock_test.rb
+++ test/knock_test.rb
@@ -1,4 +1,9 @@
 require 'test_helper'
 
 class KnockTest < ActiveSupport::TestCase
+  test 'setup block yields self' do
+    Knock.setup do |config|
+      assert_equal Knock, config
+    end
+  end
 end
