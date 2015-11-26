2fd0405d61e63321ddfac20807592f36993bda5e
stantoncbradley <stantoncbradley@gmail.com>
2015-11-25 22:17:49 -0800

    add parenthesis

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -4,7 +4,7 @@ module Knock::Authenticable
   def authenticate
     begin
       token = request.headers['Authorization'].split(' ').last
-      pubkey = File.exists? 'config/pubkey.pem' ?
+      pubkey = File.exists?('config/pubkey.pem') ?
                               OpenSSL::PKey::RSA.new(File.read('config/pubkey.pem')) :
                               nil
       @current_user = Knock::AuthToken.new(token: token, decrypt_key: pubkey).current_user
