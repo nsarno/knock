48f89781291fd8463c6f4400297c20901020a0e0
stantoncbradley <stantoncbradley@gmail.com>
2015-11-25 22:46:46 -0800

    refactor to change pub key to argument

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,13 +1,10 @@
 module Knock::Authenticable
   attr_reader :current_user
 
-  def authenticate
+  def authenticate(key: nil)
     begin
       token = request.headers['Authorization'].split(' ').last
-      pubkey = File.exists?('config/pubkey.pem') ?
-                              OpenSSL::PKey::RSA.new(File.read('config/pubkey.pem')) :
-                              nil
-      @current_user = Knock::AuthToken.new(token: token, decrypt_key: pubkey).current_user
+      @current_user = Knock::AuthToken.new(token: token, decrypt_key: key).current_user
     rescue
       head :unauthorized
     end
