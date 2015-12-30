9b172c5a0f61db6c8e168df3c40f4d5735f5e4a9
anderslemke <mail@anderslemke.dk>
2015-12-30 09:25:47 +0100

    Replace the split, and be more specific

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,7 +1,9 @@
 module Knock::Authenticable
   def current_user
     @current_user ||= begin
-      token = params[:token] || request.headers['Authorization'].split(' ').last
+      token = params[:token] ||
+        request.headers['Authorization'].match(/^Bearer (.*)$/)[1]
+
       Knock::AuthToken.new(token: token).current_user
     rescue
       nil
