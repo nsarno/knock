802020ef531b2fe667352d89fa277ec4ae46049b
Francisco Temudo <fthemudo@gmail.com>
2016-02-18 16:32:35 +0000

    Returns 401 on auth failure

--- test/controllers/knock/auth_token_controller_test.rb
+++ test/controllers/knock/auth_token_controller_test.rb
@@ -10,14 +10,14 @@ module Knock
       @user ||= users(:one)
     end
 
-    test "responds with 404 if user does not exist" do
+    test "responds with 401 if user does not exist" do
       post :create, auth: { email: 'wrong@example.net', password: '' }
-      assert_response :not_found
+      assert_response :unauthorized
     end
 
-    test "responds with 404 if password is invalid" do
+    test "responds with 401 if password is invalid" do
       post :create, auth: { email: user.email, password: 'wrong' }
-      assert_response :not_found
+      assert_response :unauthorized
     end
 
     test "responds with 201" do
