c11d49920464d3f14f8d62094b4dad3ca4b19ba0
Adones Pitogo <pitogo.adones@gmail.com>
2016-11-28 04:06:59 +0800

    Update authenticable.rb
    Fix for error:
    
         Failure/Error: authenticate_for Unsakini::User
         
         NameError:
           `@_current_unsakini/user' is not allowed as an instance variable name

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,6 +1,6 @@
 module Knock::Authenticable
   def authenticate_for entity_class
-    getter_name = "current_#{entity_class.to_s.underscore}"
+    getter_name = "current_#{entity_class.to_s.parameterize.underscore}"
     define_current_entity_getter(entity_class, getter_name)
     public_send(getter_name)
   end
