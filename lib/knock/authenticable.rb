5d846199040d2f3785453efb822e0badf85df036
Alessandro Desantis <desa.alessandro@gmail.com>
2017-01-02 13:26:50 +0100

    Add JWT::DecodeError to the rescued exceptions in Authenticable

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -46,7 +46,7 @@ module Knock::Authenticable
           current =
             begin
               Knock::AuthToken.new(token: token).entity_for(entity_class)
-            rescue ActiveRecord::RecordNotFound
+            rescue ActiveRecord::RecordNotFound, JWT::DecodeError
               nil
             end
           instance_variable_set(memoization_var_name, current)
