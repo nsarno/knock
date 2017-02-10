7799633407c5d9971c1191d3316f8ca660d0f95c
Arnaud Mesureur <arnaud.mesureur@gmail.com>
2017-02-10 18:57:59 +1100

    Check token presense before trying to look up entity

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   post 'admin_token' => 'admin_token#create'
   post 'vendor_token' => 'vendor_token#create'
 
-  resources :protected_resources
   resource :current_user
 
   resources :admin_protected
   resources :composite_name_entity_protected
-  resources :vendor_protected
   resources :custom_unauthorized_entity
-
-  mount Knock::Engine => "/knock"
+  resources :guest_protected
+  resources :protected_resources
+  resources :vendor_protected
 end
