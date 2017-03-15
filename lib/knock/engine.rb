7367258822758680427f2ed0612422402dc11b78
Vlatko Ristovski <ristovski.vlatko234@gmail.com>
2017-03-15 08:22:59 +0100

    Rails 5 forbids auto loading in production (#129)

--- lib/knock/engine.rb
+++ lib/knock/engine.rb
@@ -1,6 +1,6 @@
 module Knock
   class Engine < ::Rails::Engine
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
     isolate_namespace Knock
   end
 end
