380ddd97790cb15547f7d37345a3dffaa22ecffc
William Kurosawa <wkurosawa@gmail.com>
2019-08-01 19:00:56 +0900

    Bump JWT dependency to 2.2.1

--- test/model/knock/auth_token_test.rb
+++ test/model/knock/auth_token_test.rb
@@ -34,7 +34,7 @@ module Knock
 
       token = AuthToken.new(payload: {sub: '1'}).token
 
-      payload, header = JWT.decode token, rsa_private.public_key, true
+      payload, header = JWT.decode token, rsa_private.public_key, true, { algorithm: 'RS256' }
       assert_equal payload['sub'], '1'
       assert_equal header['alg'], 'RS256'
     end
