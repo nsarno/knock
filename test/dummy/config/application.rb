b7dabfef2f4aaf4ae1cffb8e5fba7f4814e84bfb
William Kurosawa <wkurosawa@gmail.com>
2019-08-01 19:42:55 +0900

    Fix sqlite deprecation warning

--- test/dummy/config/application.rb
+++ test/dummy/config/application.rb
@@ -19,6 +19,8 @@ module Dummy
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
+
     unless Gem.loaded_specs["rails"].version.to_s =~ /^5/
       # Do not swallow errors in after_commit/after_rollback callbacks.
       config.active_record.raise_in_transactional_callbacks = true
