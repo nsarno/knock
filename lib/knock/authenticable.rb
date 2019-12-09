32be7a7bb2d92826f3a28829e4afb2a6cb21fdb6
Arnaud Mesureur <arnaud.mesureur@gmail.com>
2019-12-10 08:35:58 +1000

    Minor refactoring of authenticable module
    - Implement `respond_to_missing?`
    - Fix some minor styling and convention issues

--- lib/knock/authenticable.rb
+++ lib/knock/authenticable.rb
@@ -1,4 +1,12 @@
+# frozen_string_literal: true
+
 module Knock
+  # Include this module in your controller to enable authentication
+  # for your endpoint.
+  #
+  # e.g.
+  # Calling `authenticate_user` will try to find a valid `User` based on
+  # the token payload.
   module Authenticable
     def authenticate_for(entity_class)
       getter_name = "current_#{entity_class.to_s.parameterize.underscore}"
@@ -24,39 +32,53 @@ module Knock
       end
     end
 
-    def authenticate_entity(entity_name)
-      if token
-        entity_class = entity_name.camelize.constantize
-        send(:authenticate_for, entity_class)
+    def respond_to_missing?(method, *)
+      prefix, = method.to_s.split("_", 2)
+      case prefix
+      when "authenticate"
+        true
+      else
+        super
       end
     end
 
-    def unauthorized_entity(entity_name)
+    def authenticate_entity(entity_name)
+      return unless token
+
+      entity_class = entity_name.camelize.constantize
+      send(:authenticate_for, entity_class)
+    end
+
+    def unauthorized_entity(_entity_name)
       head(:unauthorized)
     end
 
     def token_from_request_headers
-      unless request.headers["Authorization"].nil?
-        request.headers["Authorization"].split.last
+      request.headers["Authorization"]&.split&.last
+    end
+
+    # Dynamically defines a method similar to the example below.
+    #
+    # def current_user
+    #   @_current_user ||= fetch_entity_from_token(User)
+    # end
+    def define_current_entity_getter(entity_class, getter_name)
+      return if respond_to?(getter_name)
+
+      memoization_var_name = "@_#{getter_name}"
+      self.class.send(:define_method, getter_name) do
+        unless instance_variable_defined?(memoization_var_name)
+          current = fetch_entity_from_token(entity_class)
+          instance_variable_set(memoization_var_name, current)
+        end
+        instance_variable_get(memoization_var_name)
       end
     end
 
-    def define_current_entity_getter(entity_class, getter_name)
-      unless self.respond_to?(getter_name)
-        memoization_var_name = "@_#{getter_name}"
-        self.class.send(:define_method, getter_name) do
-          unless instance_variable_defined?(memoization_var_name)
-            current =
-              begin
-                Knock::AuthToken.new(token: token).entity_for(entity_class)
-              rescue Knock.not_found_exception_class, JWT::DecodeError, JWT::EncodeError
-                nil
-              end
-            instance_variable_set(memoization_var_name, current)
-          end
-          instance_variable_get(memoization_var_name)
-        end
-      end
+    def fetch_entity_from_token(entity_class)
+      Knock::AuthToken.new(token: token).entity_for(entity_class)
+    rescue Knock.not_found_exception_class, JWT::DecodeError, JWT::EncodeError
+      nil
     end
   end
 end
